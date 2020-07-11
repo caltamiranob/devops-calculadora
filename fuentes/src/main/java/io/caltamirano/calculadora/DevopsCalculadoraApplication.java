fa5dc01035e1de5da58abb72afd6675e124f0780
Nathalie Vasquez <nathalie15155@gmail.com>
2020-07-11 10:10:53 -0500

    Creación estructura inicial proyecto calculadora
    Se crea la estructura del proyecto calculadora con spring boot y maven
    
    Historia de usuario: 234

---
+++ fuentes/src/main/java/io/caltamirano/calculadora/DevopsCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package io.caltamirano.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCalculadoraApplication.class, args);
+	}
+
+}
